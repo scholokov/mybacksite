d44f2b4718b182d57ef2e282e67987f056f39927
Denis Scholokov <scholokov@users.noreply.github.com>
2018-12-23 11:44:51 +0200

    23

--- public/index.php
+++ public/index.php
@@ -1,10 +1,10 @@
-<?="New Text"?>
+<?= "New Text" ?>
 
 <?php 
 
 print_r("hello_1");
 
-echo("hello_echo <br>
+echo ("hello_echo <br>
 kdokok<br>
 ;ldld<br>
 ;;d f");
@@ -23,25 +23,62 @@ echo __FILE__;
 $array = array(
     "foo" => "bar",
     "bar" => "foo",
-    100   => -100,
-    -100  => 100,
+    100 => -100,
+    -100 => 100,
 );
 var_dump($array);
 
 $array = array("foo", "bar", "hallo", "world");
 var_dump($array);
 
-echo "<br><br><br><br><br><br><br><br><br><br><br>";
+echo "<br>1<br><br><br><br><br><br><br><br><br><br>";
+
+// многострочная переменная. EOT - метка, любое значение
+$message = <<<EOT
+f
+f
+f
+f  $%dd
+f<br>
+EOT;
+
+
+echo $message;
+
+echo "<br>1<br><br><br><br><br><br><br><br><br><br>";
 
 $array = array(
     "a",
     "b",
-6 => "c",
+    6 => "c",
     "d",
 );
 var_dump($array);
 
 echo "<br><br><br>";
+?>
+
+<form class="cf" method="POST">
+ <div class="half left cf">
+   <input type="text" id="input-name" placeholder="username" name="username">
+   <input type="email" id="input-email" placeholder="Email address" name="email">
+   <input type="text" id="input-subject" placeholder="Subject" name="subject">
+ </div>
+ <div class="half right cf">
+   <textarea name="message" type="text" id="input-message" placeholder="Message"></textarea>
+ </div> 
+ <input type="submit" value="Submit" id="input-submit">
+</form>
+
+
+<?php
+// if (!empty($_POST)) {
+//     echo 'Привет ' . htmlspecialchars($_POST["username"]) . '!';
+//     mail ( $_POST["email"], $_POST["subject"], $_POST["message"]);
+// } else {
+//     echo "nothing<br>";
+// }
+
 
 
 require_once '../bootstrap/bootstrap.php';
